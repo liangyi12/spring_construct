99563ec8d581ce4b858ae4db8214d6342c14f38c
liyanligithub123 <1220130039@qq.com>
2018-06-21 16:27:47 +0800

    添加注释

--- spring_test/src/main/java/org/litespring/beans/factory/BeanDefinitionStoreException.java
+++ spring_test/src/main/java/org/litespring/beans/factory/BeanDefinitionStoreException.java
@@ -2,14 +2,15 @@ package org.litespring.beans.factory;
 
 import org.litespring.beans.BeansException;
 
+/**
+ * 
+ * 存储bean定义exception
+ *
+ */
 public class BeanDefinitionStoreException extends BeansException{
 
 	public BeanDefinitionStoreException(String msg, Throwable cause) {
 		super(msg, cause);
-	}
-
-	public BeanDefinitionStoreException(String msg) {
-		super(msg);
 	}
 	
 	
