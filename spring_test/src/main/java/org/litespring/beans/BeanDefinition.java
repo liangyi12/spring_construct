2795ac8755e3435d0b694a1dfe2318175da22f1c
liyanligithub123 <1220130039@qq.com>
2018-06-21 16:26:19 +0800

    增加了scope的处理

--- spring_test/src/main/java/org/litespring/beans/BeanDefinition.java
+++ spring_test/src/main/java/org/litespring/beans/BeanDefinition.java
@@ -1,7 +1,18 @@
 package org.litespring.beans;
-
+//接口变量默认为public static final， 方法默认为public abstract
+//bean定义接口
 public interface BeanDefinition {
+
+	public static final String SCOPE_DEFAULT = "";
+	public static final String SCOPE_SINGLETON = "singleton";
+	public static final String SCOPE_PROTPTYPE = "prototype";
+	
+	public boolean isSingleton();
+	public boolean isPrototype();
+	String getScope();
+	void setScope(String scope);
 
 	String getBeanClassName();
 
+	
 }
