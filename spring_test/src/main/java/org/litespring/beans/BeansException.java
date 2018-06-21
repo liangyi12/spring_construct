508ca676221048b322396b86038215405e9ae230
liyanligithub123 <1220130039@qq.com>
2018-06-21 16:26:52 +0800

    加了注释

--- spring_test/src/main/java/org/litespring/beans/BeansException.java
+++ spring_test/src/main/java/org/litespring/beans/BeansException.java
@@ -1,5 +1,7 @@
 package org.litespring.beans;
-
+/**
+ * 自定义bean的顶级exception，继承RuntimeException，不强制捕获
+ * */
 public class BeansException extends RuntimeException{
 	public BeansException(String msg) {
 		super(msg);
