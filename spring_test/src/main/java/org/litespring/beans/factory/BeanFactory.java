f639404178a0ca7ea2069b2f75ba1b15b4cf2fd6
liyanligithub123 <1220130039@qq.com>
2018-06-21 16:29:44 +0800

    添加注释

--- spring_test/src/main/java/org/litespring/beans/factory/BeanFactory.java
+++ spring_test/src/main/java/org/litespring/beans/factory/BeanFactory.java
@@ -1,9 +1,12 @@
 package org.litespring.beans.factory;
 
-import org.litespring.beans.BeanDefinition;
-
+/**
+ * 
+ * bean工厂，提供了获取bean的方法。
+ *
+ */
 public interface BeanFactory {
-
+	/**根据beanid得到bean的实例对象*/
 	Object getBean(String beanId);
 
 }
