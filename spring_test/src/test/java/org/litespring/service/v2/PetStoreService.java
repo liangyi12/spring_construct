6a417599b40d9867fbcf2c41a9fd29796d740b95
liangyi12 <1220130039@qq.com>
2018-06-25 17:35:45 +0800

    修改测试bean

--- spring_test/src/test/java/org/litespring/service/v2/PetStoreService.java
+++ spring_test/src/test/java/org/litespring/service/v2/PetStoreService.java
@@ -4,6 +4,8 @@ public class PetStoreService {
 	private AccountDao accountDao;
 	private ItemDao itemDao;
 	private String test;
+	private int version;
+	private boolean power_switch;
 	
 	public AccountDao getAccountDao() {
 		return accountDao;
@@ -22,6 +24,18 @@ public class PetStoreService {
 	}
 	public void setTest(String test) {
 		this.test = test;
+	}
+	public int getVersion() {
+		return version;
+	}
+	public void setVersion(int version) {
+		this.version = version;
+	}
+	public boolean isPower_switch() {
+		return power_switch;
+	}
+	public void setPower_switch(boolean power_switch) {
+		this.power_switch = power_switch;
 	}
 	
 	
